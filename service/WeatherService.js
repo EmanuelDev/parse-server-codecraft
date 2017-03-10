1451df230dd86fe0477c7cda1e3e31b3b38301a7
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-10 21:20:41 +0200

    return responseadadada

--- service/WeatherService.js
+++ service/WeatherService.js
@@ -4,11 +4,11 @@ var Client = require('node-rest-client').Client;
 
 module.exports.getCluj = function () {
     var client = new Client();
-    client.registerMethod("getWeatherCluj", "http://api.openweathermap.org/data/2.5/weather?q=Cluj-Napoca,ro&APPID=65dd2ac498f766ee2c834bdea819356a", "GET");
+    client.registerMethod("getWeather", "http://api.openweathermap.org/data/2.5/weather?q=Cluj-Napoca,ro&APPID=65dd2ac498f766ee2c834bdea819356a", "GET");
 
     var result = new Parse.Promise();
 
-    client.methods.getWeatherCluj().then(function (response) {
+    client.methods.getWeather().success(function (response) {
         result.resolve(response);
     }, function(error) {
         result.reject(error);
