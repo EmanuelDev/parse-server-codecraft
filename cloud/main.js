04f1724c9db13fb0afb0b823179c8ad701e373ca
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-10 14:44:00 +0200

    set up api call

--- cloud/main.js
+++ cloud/main.js
@@ -1,1 +1,39 @@
-require("./functions/addAge");
+var https = require('https');
+var apiKey = "65dd2ac498f766ee2c834bdea819356a";
+var queryString = "q=Cluj-Napoca,ro&APPID=";
+var apiHost = "api.openweathermap.org/data/2.5/weather";
+
+function performRequest (endpoint, method, data, success) {
+    var dataString = JSON.stringify(data);
+    var headers = {};
+
+    if (method === 'GET') {
+        endpoint += '?' + queryString.stringify(data) + apiKey;
+    } else {
+        headers = {
+            'Content-Type': 'application/json',
+            'Content-Length': dataString.length
+        };
+    }
+    var options = {
+        host: apiHost,
+        path: endpoint,
+        method: method,
+        headers: headers
+    };
+
+    var req = https.request(options, function(res) {
+        res.setEncoding('utf-8');
+
+        var responseString = '';
+
+        res.on('end', function() {
+            console.log(responseString);
+            var responseObject = JSON.parse(responseString);
+            success(responseObject);
+        });
+    });
+
+    req.write(dataString);
+    req.end();
+}
