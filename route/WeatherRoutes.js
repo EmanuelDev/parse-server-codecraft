a885780f8f3071388eafa0698b767c34b2d94591
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-10 20:39:18 +0200

    Return promise on request bla bla bla bla.

--- route/WeatherRoutes.js
+++ route/WeatherRoutes.js
@@ -4,11 +4,8 @@ var weatherService = require('../service/WeatherService.js');
 
 Parse.Cloud.define("getWeatherCluj", function (request, response) {
 
-    var weatherClujResponse = weatherService.getCluj().then(function() {
-        response.success(weatherClujResponse);
-    }, function(err) {
-        console.log(err);
-    });
+    var weatherClujResponse = weatherService.getCluj();
 
+    response.success(weatherClujResponse);
 
 });
--- service/WeatherService.js
+++ service/WeatherService.js
@@ -4,16 +4,20 @@ var Client = require('node-rest-client').Client;
 
 module.exports.getCluj = function () {
     var client = new Client();
-    var result = new Parse.Promise();
     client.registerMethod("getWeatherCluj", "http://api.openweathermap.org/data/2.5/weather?q=Cluj-Napoca,ro&APPID=65dd2ac498f766ee2c834bdea819356a", "GET");
 
-    return client.methods.getWeatherCluj(function (data, response) {
+    return new Parse.Promise(function (resolve) {
+        client.methods.getWeatherCluj(function (data, response) {
 
-        result.resolve(response);
-        // parsed response body as js object
-        console.log(data);
-        // raw response
-        console.log(response);
-    });
+            response.success(function() {
+                resolve();
+            });
+            // parsed response body as js object
+            console.log(data);
+            // raw response
+            console.log(response);
+        });
+
+    })
 
 };
