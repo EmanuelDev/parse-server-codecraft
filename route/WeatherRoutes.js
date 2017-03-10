667e21be254e9b3bf528fc2dd8da08cff496e184
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-10 21:29:30 +0200

    alolcacaca

--- route/WeatherRoutes.js
+++ route/WeatherRoutes.js
@@ -4,15 +4,13 @@ var weatherService = require('../service/WeatherService.js');
 
 Parse.Cloud.define("getWeatherCluj", function (request, response) {
     var weatherClujResponse = undefined;
-    weatherService.getCluj().then(function(response) {
-        weatherClujResponse = response;
+    weatherService.getCluj().then(function(data) {
+        weatherClujResponse = data;
 
-        console.log(response, "//////////////////////////////////////////////////////////////////////////////////////////////////////////////")
+        console.log(data, "//////////////////////////////////////////////////////////////////////////////////////////////////////////////")
 
-
+        response.success(weatherClujResponse);
 
     });
-
-    response.success(weatherClujResponse);
 
 });
