110230413f613250fb94ed723124dc13e2fe27ed
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-10 20:44:34 +0200

    Return promise on request bla.

--- service/WeatherService.js
+++ service/WeatherService.js
@@ -6,16 +6,11 @@ module.exports.getCluj = function () {
     var client = new Client();
     client.registerMethod("getWeatherCluj", "http://api.openweathermap.org/data/2.5/weather?q=Cluj-Napoca,ro&APPID=65dd2ac498f766ee2c834bdea819356a", "GET");
 
-    return new Parse.Promise(function (resolve) {
-        client.methods.getWeatherCluj(function (data, response) {
+    var result = new Parse.Promise();
 
-            response.json(data);
-            // parsed response body as js object
-            console.log(data);
-            // raw response
-            console.log(response);
-        });
+    client.methods.getWeatherCluj(function (data, response) {
 
-    })
+        result.resolve(data);
 
+    });
 };
