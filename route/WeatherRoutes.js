010d3b446ad0c8c78304c5fd51564ef7deb4c566
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-10 18:14:43 +0200

    Return promise on request.

--- route/WeatherRoutes.js
+++ route/WeatherRoutes.js
@@ -5,7 +5,6 @@ var weatherService = require('../service/WeatherService.js');
 Parse.Cloud.define("getWeatherCluj", function (request, response) {
 
     var weatherClujResponse = weatherService.getCluj();
-    console.log(weatherClujResponse);
-    response.success("caca");
+    response.success(weatherClujResponse);
 
 });
