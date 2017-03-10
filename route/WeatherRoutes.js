2b517b0e781732b42bf961db98e03f9bc729fcbf
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-10 17:57:06 +0200

    Caca

--- route/WeatherRoutes.js
+++ route/WeatherRoutes.js
@@ -5,6 +5,7 @@ var weatherService = require('../service/WeatherService.js');
 Parse.Cloud.define("getWeatherCluj", function (request, response) {
 
     var weatherClujResponse = weatherService.getCluj();
-    response.success(weatherClujResponse);
+    console.log(weatherClujResponse);
+    response.success("caca");
 
 });
