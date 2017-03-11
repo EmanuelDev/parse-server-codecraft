1f6a3788eac3d8fccd0871e72d97192730dfbfd7
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-11 15:46:20 +0200

    parse gaca

--- service/WeatherService.js
+++ service/WeatherService.js
@@ -7,14 +7,15 @@ module.exports.getClujWeather = function () {
     client.registerMethod("getWeather", "http://api.openweathermap.org/data/2.5/weather?q=Cluj-Napoca,ro&APPID=65dd2ac498f766ee2c834bdea819356a", "GET");
 
     var result = new Parse.Promise();
-    var Weather = Parse.Object.extend('WeatherCluj');
-    var weatherCluj = new Weather();
+    var WeatherCluj = Parse.Object.extend('WeatherCluj');
+    var weatherCluj = new WeatherCluj();
 
     client.methods.getWeather(function (response) {
         weatherCluj.set("cityName", response.name);
         weatherCluj.set("country", response.sys.country);
         // weatherCluj.set("description", response.weather["description"].description);
         weatherCluj.set("windSpeed", response.wind.speed);
+        weatherCluj.save();
         result.resolve(response);
     }, function (err) {
         result.reject(error);
