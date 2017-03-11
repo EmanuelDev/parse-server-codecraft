fb0bab00d39222958f2d7560ab80cdff32e1ac00
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-11 16:14:58 +0200

    check if bg job works

--- jobs/BackgroundJob.js
+++ jobs/BackgroundJob.js
@@ -3,25 +3,10 @@
  */
 var https = require('https');
 var cron = require('node-cron');
+var weatherService = require('../service/WeatherService.js');
 
 var task = cron.schedule('*/10 * * * *', function() {
-    var options = {
-        host: 'api.openweathermap.org',
-        port: 80,
-        path: 'data/2.5/weather?q=Cluj-Napoca,ro&APPID=65dd2ac498f766ee2c834bdea819356a',
-        method: 'GET'
-    };
-
-    https.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function (body) {
-            console.log('BODY: ' + body);
-            // var Weather = Parse.Object.extend("Weather");
-            // var weather = new Weather();
-        });
-    }).end();
+    cosnole.log("cevaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
 });
 
