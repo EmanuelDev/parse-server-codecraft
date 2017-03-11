2b81151cb60e31f54e28b6113b3311942d2dd055
Emanuel Borbely <emaborbely19@gmail.com>
2017-03-11 14:48:47 +0200

    add to database test.

--- route/WeatherRoutes.js
+++ route/WeatherRoutes.js
@@ -5,17 +5,9 @@ var weatherService = require('../service/WeatherService.js');
 Parse.Cloud.define("getWeatherCluj", function (request, response) {
     var weatherClujResponse = undefined;
 
-    // var Weather = Parse.Object.extend('WeatherCluj');
-    //
-    // var weatherCluj = new Weather();
-
     weatherService.getClujWeather().then(function(data) {
 
-        // weatherClujResponse = data;
-        // weatherCluj.set("cityName", data.result.name);
-        // weatherCluj.set("country", data.result.sys.country);
-        // weatherCluj.set("description", data.result.weather.description);
-        // weatherCluj.set("windSpeed", data.result.wind.speed);
+        weatherClujResponse = data;
 
         response.success(weatherClujResponse);
 
--- service/WeatherService.js
+++ service/WeatherService.js
@@ -7,9 +7,14 @@ module.exports.getClujWeather = function () {
     client.registerMethod("getWeather", "http://api.openweathermap.org/data/2.5/weather?q=Cluj-Napoca,ro&APPID=65dd2ac498f766ee2c834bdea819356a", "GET");
 
     var result = new Parse.Promise();
+    var Weather = Parse.Object.extend('WeatherCluj');
+    var weatherCluj = new Weather();
 
     client.methods.getWeather(function (response) {
-        console.log(response, "//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+        weatherCluj.set("cityName", response.name);
+        weatherCluj.set("country", response.sys.country);
+        weatherCluj.set("description", response.weather["description"].description);
+        weatherCluj.set("windSpeed", response.wind.speed);
         result.resolve(response);
     }, function (err) {
         result.reject(error);
